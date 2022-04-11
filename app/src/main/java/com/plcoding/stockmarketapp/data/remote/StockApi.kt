1ebd1af192c19c4bca33ab925ae1964274aa5522
David <madunts.d@gmail.com>
2022-04-11 12:19:34 +0500

    Initial setup

---
+++ app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
@@ -0,0 +1,16 @@
+package com.plcoding.stockmarketapp.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StockApi {
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListings(
+        @Query("apiKey") apiKey: String
+    ): ResponseBody
+
+    companion object {
+        const val BASE_URL = "https://alphavantage.co"
+    }
+}
